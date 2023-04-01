2921f3f03b5bd22ad0fb4b2c0c561a04c7758a48
Ayaka Mikazuki <ayaka@mail.shn.hk>
2023-04-01 22:29:23 +1000

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Uyghur Online Practice System
+
+## What is this?
+
+Uyghur Online Practice System is a web-based platform that provides users with the opportunity to practice the Uyghur Arabic script. This system presents users with a Uyghur word written in Arabic script, along with its Chinese translation. Users are then prompted to type the corresponding Uyghur Latin script according to the Arabic script, providing an exercise designed to improve Uyghur literacy skills.
+
+## Significance
+
+This project is particularly significant given the fact that Uyghur is an official language of the Xinjiang Uyghur Autonomous Region with 8-11 million speakers. It is widely used in both social and official spheres, as well as in print, television, and radio; and is used as a common language by other ethnic minorities in Xinjiang. By making resources like the Uyghur Online Practice System available, we aim to support the preservation of Uyghur culture and language.
+
+Additionally, Uyghur is a low-resource language, which means that there is a limited amount of language processing and computing resources available for the language. By developing this Uyghur Online Practice System, we hope to contribute to the development of Uyghur language resources and promote the use and understanding of this unique and rich language.
+
+## Contribute
+
+If you are interested in contributing to this project, we encourage you to do so through GitHub. You can create pull requests and contribute to the development of the system, helping to improve its functionality and expand its capabilities. Whether you are a Uyghur speaker or just interested in supporting the preservation of this language, your contributions are valuable and appreciated.
+
+## Acknowledgements
+
+The word list is taken from [iamironrabbit/monlam-dictionary](https://github.com/colordict/colordict.github.io/blob/a402ec135da921771135c63b952eb9d827c995e1/stardict-anatilim-uyghur-chinese_uy-cn-2.4.2.tar.gz).
---
+++ src/App.js
@@ -0,0 +1,152 @@
+import React, { Component } from "react";
+import randomWord from "./randomWord";
+
+const toULS = (s) =>
+  [...s]
+    .map(
+      (c) =>
+        ({
+          ا: "a",
+          ب: "b",
+          س: "s",
+          د: "d",
+          ف: "f",
+          گ: "g",
+          ى: "i",
+          ج: "j",
+          ك: "k",
+          ل: "l",
+          م: "m",
+          ن: "n",
+          و: "o",
+          ۆ: "ö",
+          پ: "p",
+          ڭ: "ng",
+          چ: "c",
+          ر: "r",
+          ت: "t",
+          ۇ: "u",
+          ۈ: "ü",
+          ۋ: "w",
+          ي: "y",
+          ز: "z",
+          ژ: "zh",
+          غ: "gh",
+          "؟": "?",
+          "ٜ": ".",
+          "،": ",",
+          ھ: "h",
+          خ: "x",
+          ق: "q",
+          ش: "sh",
+          ە: "e",
+          ې: "ë",
+          ئ: "'",
+        }[c])
+    )
+    .join("")
+    .replace(/^'/, "");
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    const [uyghur, meaning] = randomWord();
+    const latin = toULS(uyghur).trim();
+
+    this.state = {
+      uyghur,
+      latin,
+      meaning,
+      hint: false,
+      userAnswer: "",
+      counter: 0,
+      uls: "",
+    };
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.userAnswer !== this.state.userAnswer) {
+      if (this.state.userAnswer === this.state.latin) {
+        const [uyghur, meaning] = randomWord();
+        const latin = toULS(uyghur);
+
+        setTimeout(() => {
+          this.setState({
+            uyghur,
+            latin,
+            meaning,
+            hint: false,
+            userAnswer: "",
+            counter: this.state.counter + 1,
+          });
+        }, 1000);
+      }
+    }
+  }
+
+  handleChange = (e) => {
+    const userAnswer = e.target.value;
+    this.setState({ userAnswer });
+  };
+
+  handleHover = (e) => {
+    const char = e.target.textContent;
+    const uls = toULS(char);
+    this.setState({ uls });
+  };
+
+  handleSpanClick = (e) => {
+    const { userAnswer } = this.state;
+    const char = e.target.textContent;
+    const uls = toULS(char);
+    this.setState({ userAnswer: userAnswer + uls });
+  };
+
+  render() {
+    const { uyghur, latin, meaning, hint, userAnswer, counter, uls } =
+      this.state;
+
+    return (
+      <React.Fragment>
+        <div className="row h-100 justify-content-center align-items-center">
+          <div className="col-lg-7 text-center main-panel">
+            <p lang="ug" dir="ltr" className="display-1 my-3 highlight">
+              {uyghur.split("").map((char, i) => (
+                <span
+                  key={i}
+                  onPointerOver={this.handleHover}
+                  onClick={this.handleSpanClick}
+                >
+                  {char}
+                </span>
+              ))}
+            </p>
+            <p lang="zh-CN">{meaning}</p>
+            <p className="margin-y-1">
+              <input
+                type="text"
+                className="form-control bg-dark text-light text-center"
+                onChange={this.handleChange}
+                value={userAnswer}
+              />
+            </p>
+          </div>
+        </div>
+        <div className="left-panel my-2 pt-2 mx-3">
+          <p>Count: {counter}</p>
+          <p>Hover hint: {JSON.stringify(uls)}</p>
+        </div>
+        <div className="right-panel my-2 mx-3">
+          <input
+            type="button"
+            className="btn btn-secondary round"
+            value="❔"
+            onClick={() => this.setState({ hint: !hint })}
+          />
+          <p className={hint ? "my-2" : "my-2 invisible"}>{latin}</p>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
